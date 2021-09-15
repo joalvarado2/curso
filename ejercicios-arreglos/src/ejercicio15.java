41c9e1e3d3ecbb3adee86496a47880fab51a98da
joalvarado2 <jonathanalva92@gmail.com>
2021-09-15 16:11:38 -0500

    ordenamiento de array 15/09/2021

--- ejercicios-arreglos/src/ejercicio15.java
+++ ejercicios-arreglos/src/ejercicio15.java
@@ -1,20 +1,36 @@
+/*
+    ordenar dos arreglos y unirlos un una sola de manera ordenada
+*/
 import java.util.Arrays;
-
 
 public class ejercicio15 {
     public static void main(String[] args) {
-        int a[] = {9, 10, 6, 3, 12, 5, 1, 98, 48};
-        int b[] = {1, 6, 32, 4, 2, 5, 12, 11, 62};
+        int a[] = {1, 9, 3, 5, 7, 11, 15, 17, 13};
+        int b[] = {4, 6, 10, 2, 8, 14, 12, 18, 16};
+        int c[] = new  int[15];
 
         Arrays.sort(a);
         System.out.println("Arreglo 1 ordenado \n"+Arrays.toString(a));
         Arrays.sort(b);
         System.out.println("Arreglo 2 ordenado \n"+Arrays.toString(b));
 
-        String union = Arrays.toString(a) + Arrays.toString(b);
-        System.out.println(union);
+        int i = 0; // iterador para el arreglo 1
+        int j = 0; // iterador para el arreglo 2
+        int k = 0; // iterador para el arreglo final
 
+        // Uniendo dos arrays en uno
+        while (i < 8 ) {
+            if (a[i] < b[j]) { // si el numero del arreglo 1 es mayor al del arreglo 2
+                c[k] = a[i]; // la posicion k toma el valor de la posicion i
+                i++; // avanzamos una posicion en el arreglo 1
+            } else {
+                c[k] = b[j];
+                j++; // avanzamos una posicion en el arreglo 2
+            }
+            k++; // avanzamos una posicion en el arreglo k
+        }
 
+        System.out.println(Arrays.toString(c));
     }
 
 }
