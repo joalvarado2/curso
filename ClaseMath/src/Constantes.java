2c853084cb459fd7c5f2a1941e3b569cb88fc301
joalvarado2 <jonathanalva92@gmail.com>
2022-04-06 21:58:53 -0500

    hilos y clase Math 06/04/2022

---
+++ ClaseMath/src/Constantes.java
@@ -0,0 +1,6 @@
+public class Constantes {
+    public static void main(String[] args) {
+        System.out.println("El numero e: " + Math.E);
+        System.out.println("El numero pi: " + Math.PI);
+    }
+}
---
+++ ClaseMath/src/Conversiones.java
@@ -0,0 +1,11 @@
+public class Conversiones {
+    public static void main(String[] args) {
+        double anguloEnGrados = 45;
+
+        double anguloEnRadianes = Math.toRadians(anguloEnGrados);
+        System.out.println("Deg a Rad " + anguloEnGrados + " = " + anguloEnRadianes + " rad");
+
+         anguloEnGrados = Math.toDegrees(anguloEnRadianes);
+        System.out.println("Deg a Rad " + anguloEnRadianes + " = " + anguloEnGrados + " grados");
+    }
+}
